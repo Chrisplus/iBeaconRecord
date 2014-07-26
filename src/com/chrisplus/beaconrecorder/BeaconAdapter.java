e9150f4bb6770112889d65c830c9e7d00c4a035b
Chris Jiang <chrisplus.jiang@gmail.com>
2014-07-26 23:15:05 +0800

    Finish ibeacon part

---
+++ src/com/chrisplus/beaconrecorder/BeaconAdapter.java
@@ -0,0 +1,136 @@
+package com.chrisplus.beaconrecorder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import android.content.Context;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.aprilbrother.aprilbrothersdk.Beacon;
+
+public class BeaconAdapter extends BaseAdapter {
+
+	private ArrayList<Beacon> beacons;
+	private LayoutInflater inflater;
+
+	public static final String SENSINGTHREAD = "Sensing Thread";
+	private FileOutputStream outstream;
+	private File file;
+
+	public BeaconAdapter(Context context) {
+		this.inflater = LayoutInflater.from(context);
+		this.beacons = new ArrayList<Beacon>();
+		File sdCard = Environment.getExternalStorageDirectory();
+		File dir = new File(sdCard.getAbsolutePath() + "/Beacon_Log/");
+		dir.mkdirs();
+		file = new File(dir, System.currentTimeMillis() + ".csv");
+	}
+
+	public void replaceWith(Collection<Beacon> newBeacons) {
+		this.beacons.clear();
+		this.beacons.addAll(newBeacons);
+		notifyDataSetChanged();
+	}
+
+	@Override
+	public int getCount() {
+		return beacons.size();
+	}
+
+	@Override
+	public Beacon getItem(int position) {
+		return beacons.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View view, ViewGroup parent) {
+		view = inflateIfRequired(view, position, parent);
+		bind(getItem(position), view);
+		return view;
+	}
+
+	private void bind(Beacon beacon, View view) {
+		ViewHolder holder = (ViewHolder) view.getTag();
+		holder.macTextView.setText(String.format("MAC: %s (%.2fm)",
+				beacon.getMacAddress(), beacon.getDistance()));
+		holder.uuidTextView.setText("UUID: " + beacon.getProximityUUID());
+		holder.majorTextView.setText("Major: " + beacon.getMajor());
+		holder.minorTextView.setText("Minor: " + beacon.getMinor());
+		holder.measuredPowerTextView.setText("MPower: "
+				+ beacon.getMeasuredPower());
+		holder.rssiTextView.setText("RSSI: " + beacon.getRssi());
+
+		// // Log.d("Running",
+		// "," + System.currentTimeMillis() + "," + beacon.getRssi() + ","
+		// + beacon.getDistance());
+
+		String temp = System.currentTimeMillis() + "," + beacon.getRssi() + ","
+				+ beacon.getDistance() + "\r\n";
+		try {
+			if (outstream == null) {
+				outstream = new FileOutputStream(file);
+			}
+
+			outstream.write(temp.getBytes());
+			outstream.flush();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private View inflateIfRequired(View view, int position, ViewGroup parent) {
+		if (view == null) {
+			view = inflater.inflate(R.layout.deviceitem, null);
+			view.setTag(new ViewHolder(view));
+		}
+		return view;
+	}
+
+	public void flush() {
+		if (outstream != null) {
+			try {
+				outstream.flush();
+				outstream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	static class ViewHolder {
+		final TextView macTextView;
+		final TextView uuidTextView;
+		final TextView majorTextView;
+		final TextView minorTextView;
+		final TextView measuredPowerTextView;
+		final TextView rssiTextView;
+
+		ViewHolder(View view) {
+			macTextView = (TextView) view.findViewWithTag("mac");
+			uuidTextView = (TextView) view.findViewWithTag("uuid");
+			majorTextView = (TextView) view.findViewWithTag("major");
+			minorTextView = (TextView) view.findViewWithTag("minor");
+			measuredPowerTextView = (TextView) view.findViewWithTag("mpower");
+			rssiTextView = (TextView) view.findViewWithTag("rssi");
+		}
+	}
+
+}
--- src/com/chrisplus/beaconrecorder/MainActivity.java
+++ src/com/chrisplus/beaconrecorder/MainActivity.java
@@ -1,15 +1,147 @@
 package com.chrisplus.beaconrecorder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.aprilbrother.aprilbrothersdk.Beacon;
+import com.aprilbrother.aprilbrothersdk.BeaconManager;
+import com.aprilbrother.aprilbrothersdk.BeaconManager.RangingListener;
+import com.aprilbrother.aprilbrothersdk.Region;
 
 public class MainActivity extends Activity {
+
+	private static final int REQUEST_ENABLE_BT = 1234;
+	private static final String TAG = "MainActivity";
+	private static final Region ALL_BEACONS_REGION = new Region("apr", null,
+			null, null);
+	private BeaconAdapter adapter;
+	private BeaconManager beaconManager;
+	private ArrayList<Beacon> myBeacons;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		init();
 	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	private void init() {
+		myBeacons = new ArrayList<Beacon>();
+		ListView lv = (ListView) findViewById(R.id.lv);
+		adapter = new BeaconAdapter(this);
+		lv.setAdapter(adapter);
+		beaconManager = new BeaconManager(this);
+		beaconManager.setRangingListener(new RangingListener() {
+
+			@Override
+			public void onBeaconsDiscovered(Region region,
+					final List<Beacon> beacons) {
+
+				myBeacons.addAll(beacons);
+
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						getActionBar().setSubtitle(
+								"Found beacons: " + beacons.size());
+						adapter.replaceWith(beacons);
+					}
+				});
+			}
+		});
+	}
+
+	private void connectToService() {
+		getActionBar().setSubtitle("Scanning...");
+		adapter.replaceWith(Collections.<Beacon> emptyList());
+		beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+			@Override
+			public void onServiceReady() {
+				try {
+					beaconManager.startRanging(ALL_BEACONS_REGION);
+				} catch (RemoteException e) {
+				}
+			}
+		});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT) {
+			if (resultCode == Activity.RESULT_OK) {
+				connectToService();
+			} else {
+				Toast.makeText(this, "Bluetooth not enabled", Toast.LENGTH_LONG)
+						.show();
+				getActionBar().setSubtitle("Bluetooth not enabled");
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		if (!beaconManager.hasBluetooth()) {
+			Toast.makeText(this, "Device does not have Bluetooth Low Energy",
+					Toast.LENGTH_LONG).show();
+			return;
+		}
+
+		if (!beaconManager.isBluetoothEnabled()) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		} else {
+			connectToService();
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		beaconManager.disconnect();
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onStop() {
+		try {
+			myBeacons.clear();
+			beaconManager.stopRanging(ALL_BEACONS_REGION);
+		} catch (RemoteException e) {
+			Log.d(TAG, "Error while stopping ranging", e);
+		}
+		super.onStop();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		if (adapter != null) {
+			adapter.flush();
+		}
+	}
+
 }
